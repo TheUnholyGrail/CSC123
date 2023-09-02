223dc8d3adb993b34093e61a8767649d48e95ceb
a7mor <a7mor@10.164.2.253>
2023-09-02 15:29:48 -0700

    -Added the actual project files

---
+++ CSC123_1/src/csc123_module/HelloWorld.java
@@ -0,0 +1,8 @@
+package csc123_module;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//Start Here
+		System.out.println("Hello World");
+	}
+}
---
+++ CSC123_1/src/csc123_module/package-info.java
@@ -0,0 +1,1 @@
+package csc123_module;
